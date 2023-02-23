0859647a59d572dc6bc3a7d7fb7956b450558c3a
carosop <108218707+carosop@users.noreply.github.com>
2023-02-23 10:25:19 +0100

    Update water_level.c

--- modules/water_level/water_level.c
+++ modules/water_level/water_level.c
@@ -2,39 +2,13 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include <ti/devices/msp432p4xx/inc/msp.h>
 
-//function
-/*String analogPinConverter(int value){
-	String analogPinValue;
-	
-	switch(value){
-		//pin D14 = A0
-		case 14: analogPinValue = "A0"; break;
-		//pin D15 = A1
-		case 15: analogPinValue = "A1"; break;
-		//pin D16 = A2
-		case 16: analogPinValue = "A2"; break;
-		//pin D17 = A3
-		case 17: analogPinValue = "A3"; break;
-		//pin D18 = A4
-		case 18: analogPinValue = "A4"; break;
-		//pin D19 = A5
-		case 19: analogPinValue = "A5"; break;
-		default :_ analogPinValue = "Pin Error"; break;
-	} 
-	return analogPinValue; 
-}*/
-
-
 //Global Constants
-//const int sensorPin = GPIO_PORT_P;
 //if TapWater there is no conductibility so values change 
 const bool tapWater = true;
  
 //Global variables
 int HistoryValue = 0;
 char printBuffer[45];
-//String analogPin;
-
 
 //Void Setup
 void _waterSensorInit(){
@@ -54,11 +28,7 @@ void _waterSensorInit(){
             ADC14_initModule(ADC_CLOCKSOURCE_MCLK, ADC_PREDIVIDER_1, ADC_DIVIDER_4, ADC_MAPINTCH1);
 
             /* Configuring ADC Memory */
-            //ADC14_configureSingleSampleMode(ADC_MEM0, true);
             ADC14_configureMultiSequenceMode(ADC_MEM0, ADC_MEM1, true);
-            //ADC14_configureSingleSampleMode(ADC_MEM1, true);
-            //ADC14_configureConversionMemory(ADC_MEM1, ADC_VREFPOS_AVCC_VREFNEG_VSS,
-            //ADC_INPUT_A1, false);
             ADC14_configureConversionMemory(ADC_MEM0, ADC_VREFPOS_AVCC_VREFNEG_VSS,
                         ADC_INPUT_A0, false);
             ADC14_configureConversionMemory(ADC_MEM1, ADC_VREFPOS_AVCC_VREFNEG_VSS,
@@ -77,7 +47,6 @@ void _waterSensorInit(){
             ADC14_enableInterrupt(ADC_INT0);
             ADC14_enableInterrupt(ADC_INT1);
             Interrupt_enableInterrupt(INT_ADC14);
-            //Interrupt_enableMaster();
 }
 
 void ADC14_IRQHandler(void)
